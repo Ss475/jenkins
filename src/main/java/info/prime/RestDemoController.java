e7c35cee6724efd55ade47cbcd4b477bcf072870
Ss475 <45161744+Ss475@users.noreply.github.com>
2019-01-08 18:14:33 +0530

    committed for arrayList import

--- src/main/java/info/prime/RestDemoController.java
+++ src/main/java/info/prime/RestDemoController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,7 +19,7 @@ public class RestDemoController {
 
 	@PostConstruct
 	public void getData() {
-		arrayList = new ArrayList<>();
+		arrayList = new ArrayList<Employee>();
 		arrayList.add(new Employee(1, "arundathi", 1234.0));
 	}
 
